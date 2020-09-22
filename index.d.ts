98bc00469741d475e0e750453e4f53dc3df20107
Matthias Giger <i@matthiasgiger.com>
2020-09-22 23:39:25 +0200

    feat(calculation): switch from rem to pixels
    Modern browsers work fine with pixel values even for zooming.
    
    BREAKING CHANGE: uses pixels instead of rem values

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,13 @@
 // Reference: https://www.typescriptlang.org/play
-export declare const configure: (scalingRatio: number) => void;
+export declare const configure: (configuration: {
+  scalingRatio: number
+  viewportMin: number
+  viewportMax: number
+  scalingRatioFont: number
+  fontSizeToLineHeightRatio: number
+}) => void
 export declare const wasser: (
   property: string,
   max: number,
   min?: number
-) => string;
+) => string
--- index.js
+++ index.js
@@ -1,29 +1,55 @@
-let _scalingRatio = 1;
-let _viewportMin = 320;
-let _viewportMax = 1500;
+const variables = {
+  scalingRatio: 1.5,
+  viewportMin: 320,
+  viewportMax: 1500,
+  scalingRatioFont: 1.25,
+  fontSizeToLineHeightRatio: 1.25,
+}
 
-const _getResponsiveProperty = (max, min, property) => `
-    ${property}: calc(${$min} + (${max} - ${min}) * (100vw - ${_viewportMin}) / (${_viewportMax} - ${_viewportMin}));
+export const configure = (configuration) =>
+  Object.assign(variables, configuration)
 
-    @media (min-width: ${_viewportMax}) {
+const getResponsiveProperty = (max, min, property) => `
+    ${property}: calc(${min} + (${max} - ${min}) * (100vw - ${
+  variables.viewportMin
+}) / (${variables.viewportMax} - ${variables.viewportMin}));
+
+    @media (min-width: ${variables.viewportMax}) {
         ${property}: ${max};
     }
 
-    @media (max-width: ${_viewportMin - 1}) {
+    @media (max-width: ${variables.viewportMin - 1}) {
         ${property}: ${min};
     }
 
     @media print {
         ${property}: ${max};
     }
-`;
+`
 
-export const configure = (scalingRatio) => (_scalingRatio = scalingRatio);
-
-export const wasser = (property, max, min = max / _scalingRatio) => {
-  if (typeof max != "number") {
-    throw new Error("wasser: A number is expected as the second parameter.");
+export const wasser = (property, max, min = max / variables.scalingRatio) => {
+  if (typeof max !== 'number') {
+    throw new Error('wasser: A number is expected as the second parameter.')
   }
 
-  _getResponsiveProperty(property, max, min);
-};
+  return getResponsiveProperty(property, max, min)
+}
+
+export const font = (
+  max,
+  min = max / variables.scalingRatioFont,
+  lineHeightRaio = variables.fontSizeToLineHeightRatio
+) => {
+  if (typeof max !== 'number' || typeof min !== 'number') {
+    throw new Error(
+      'wasser font(): A number is expected as the first and second parameter.'
+    )
+  }
+
+  return `${getResponsiveProperty('font-size', max, min)}
+  ${getResponsiveProperty(
+    'line-height',
+    max * lineHeightRaio,
+    min * lineHeightRaio
+  )}`
+}
